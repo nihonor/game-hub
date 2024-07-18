f02bf13314e1c235724991b45522fb4d34d0797d
nihonor <niyitanganihonor@gmail.com>
2024-07-18 15:00:39 +0200

    displaying critic score

--- hospital/src/component/GameCard.tsx
+++ hospital/src/component/GameCard.tsx
@@ -1,7 +1,8 @@
 import { Game } from "../hooks/useGames";
-import { Image, Card, Heading, CardBody, Text } from "@chakra-ui/react";
+import { Image, Card, Heading, CardBody, Text, HStack } from "@chakra-ui/react";
 import getCroppedImageUrl from "../services/image-url";
 import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 
 interface Props {
   game: Game;
@@ -11,10 +12,14 @@ const GameCard = ({ game }: Props) => {
     <Card borderRadius={10} overflow="hidden">
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
+        <Heading fontSize="xl">{game.name}</Heading>
+        <HStack justifyContent='space-between'>
         <PlatformIconList
           platforms={game.parent_platforms.map((p) => p.platform)}
         />
+        <CriticScore score={game.metacritic}/>
+        </HStack>
+        
       </CardBody>
     </Card>
   );
