65fddc9d7345573439f26d5d62fc51915318f743
nihonor <niyitanganihonor@gmail.com>
2024-07-18 14:48:21 +0200

    clean up the genres

--- hospital/src/component/GenreList.tsx
+++ hospital/src/component/GenreList.tsx
@@ -1,16 +1,19 @@
-import { HStack, List, ListItem, Image, Text } from "@chakra-ui/react";
+import { HStack, List, ListItem, Image, Text, Heading } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data } = useGenres();
   return (
+    <>
+    <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
     <List>
       {data.map((genre) => (
         <ListItem key={genre.id} paddingY='5px'>
           <HStack>
             <Image
               boxSize="32px"
+              objectFit='cover'
               borderRadius={8}
               src={getCroppedImageUrl(genre.image_background) }
             />
@@ -19,6 +22,7 @@ const GenreList = () => {
         </ListItem>
       ))}
     </List>
+    </>
   );
 };
 
